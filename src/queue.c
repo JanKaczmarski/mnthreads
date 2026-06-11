68c203ee8759485887d121a02ea4b12a4d027d7a
jkaczmarski <jan.kaczmarski.dev@gmail.com>
2026-06-11 08:40:45 +0000

    implement spinlock and thread queue

--- src/queue.c
+++ src/queue.c
@@ -1,52 +1,65 @@
 #include "queue.h"
 #include "tcb.h"
 #include <stddef.h>
+#include <time.h>
 
 void queue_init(thread_queue_t *q)
 {
-    /* TODO: Set head, tail to NULL, size to 0. */
-    (void)q;
+    q->head = NULL;
+    q->tail = NULL;
+    q->size = 0;
 }
 
 void queue_push(thread_queue_t *q, tcb_t *t)
 {
-    /*
-     * TODO: Append t to the tail of the list.
-     * Set t->next = NULL.
-     * If the queue was empty, head = tail = t.
-     * Otherwise, tail->next = t; tail = t.
-     * Increment size.
-     */
-    (void)q;
-    (void)t;
+    // last element in thread queue
+    t->next = NULL;
+
+    if (q->head == NULL) {
+        q->head = q->tail = t;
+    } else {
+        q->tail->next = t;
+        q->tail = t;
+    }
+
+    q->size++;
 }
 
 tcb_t *queue_pop(thread_queue_t *q)
 {
-    /*
-     * TODO: Remove and return the head.
-     * If empty, return NULL.
-     * If head == tail, set both to NULL.
-     * Decrement size.
-     */
-    (void)q;
-    return NULL;
+    if (q->head == NULL) {
+        return NULL;
+    }
+
+    tcb_t* t = q->head;
+
+    q->head = t->next;
+
+    if (q->head == NULL) {
+        q->tail = NULL;
+    }
+
+    t->next = NULL;
+    q->size--;
+
+    return t;
 }
 
 tcb_t *queue_peek(const thread_queue_t *q)
 {
-    (void)q;
-    return NULL;
+    if (q->head == NULL) {
+        return NULL;
+    }
+
+    return q->head;
 }
 
 bool queue_empty(const thread_queue_t *q)
 {
-    (void)q;
-    return true;
+    return q->head == NULL;
 }
 
 int queue_size(const thread_queue_t *q)
 {
-    (void)q;
-    return 0;
+    return q->size;
 }
--- src/spinlock.c
+++ src/spinlock.c
@@ -1,9 +1,9 @@
 #include "spinlock.h"
+#include "stdatomic.h"
 
 void spinlock_init(spinlock_t *lock)
 {
-    /* TODO: Clear the atomic flag. */
-    (void)lock;
+    atomic_flag_clear(&lock->flag);
 }
 
 void spinlock_lock(spinlock_t *lock)
@@ -15,7 +15,11 @@ void spinlock_lock(spinlock_t *lock)
      * Optionally add a pause/yield hint in the spin loop
      * to reduce bus contention (e.g., __builtin_ia32_pause()).
      */
-    (void)lock;
+
+    // spin-wait while mutex is taken
+    while(atomic_flag_test_and_set_explicit(&lock->flag, memory_order_acquire) == 1);
+
+    // mutex aquired above
 }
 
 void spinlock_unlock(spinlock_t *lock)
@@ -23,5 +27,5 @@ void spinlock_unlock(spinlock_t *lock)
     /*
      * TODO: Clear the flag with memory_order_release.
      */
-    (void)lock;
+    atomic_flag_clear_explicit(&lock->flag, memory_order_release);
 }
